377cc0aacc0d5c4429c22c89b12f9b70b891e78d
Ashwin Kanhere <kanhereashwin@users.noreply.github.com>
2019-04-18 14:49:48 -0500

    Added small block to test functions from ndt.py

--- ashwin-playground.py
+++ ashwin-playground.py
@@ -4,3 +4,18 @@ File to test different modules and functions created for the offline implementat
 Author: Ashwin Kanhere
 Date Modified: 15th April 2019
 """
+import numpy as np
+import pykitti
+import pptk
+import ndt
+
+basedir = 'D:\\Users\\kanhe\\Box Sync\\RA Work\\ION GNSS 19\\Implementation\\Dataset'
+date = '2011_09_26'
+drive = '0005'
+
+data = pykitti.raw(basedir, date, drive, frames= range(0, 5, 1))
+
+points_lidar = data.velo
+test_lidar = data.get_velo(0) # LiDAR point cloud is a Nx4 numpy array
+#test_cloud = ndt_approx.ndt_approx(test_lidar)
+testing = ndt.pc_to_ndt()
