7ed2bc5faaa3f3ef43289c06001b3a134fe65c70
Ashwin Kanhere <kanhereashwin@users.noreply.github.com>
2019-05-31 13:25:48 +0530

    Added files and TODOs for verification of odometry results

---
+++ function_verification.py
@@ -0,0 +1,12 @@
+"""
+function_verification.py
+File containing test cases to verify whether functions are working as they're supposed to or not
+Author: Ashwin Kanhere
+Date created: 30th May 2019
+"""
+
+# TODO: Review code for rotation matrix function
+# TODO: Review code for dcm2eul
+# TODO: Review code for eul2dcm
+# TODO: Review code for transform_pts
+
---
+++ kitti_verify.py
@@ -0,0 +1,8 @@
+"""
+kitti_verify.py
+Test cases and functions to compare algorithm performance to KITTI ground truths
+Author: Ashwin Kanhere
+Date created: 31st May 2019
+"""
+
+# TODO: Verify odometry results
