d88f8fb3b05abcb2bc41abacd00dddd4064b08db
Ashwin Kanhere <kanhereashwin@users.noreply.github.com>
2019-06-13 16:57:57 -0500

    First commit for mapping.py

---
+++ mapping.py
@@ -0,0 +1,7 @@
+"""
+mapping.py
+Functions to update the map and perform global optimization given for a keyframe of NDT Clouds
+Author: Ashwin Kanhere
+Date created: 13th June 2019
+Last modified: 13th June 2019
+"""
