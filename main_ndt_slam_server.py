b0a4ccc8b133dba1834eaefce0fb9ef0fc3243d5
mjp5578 <peretic2@illinois.edu>
2019-06-16 23:23:36 -0400

    Initial commit of server main file

---
+++ main_ndt_slam_server.py
@@ -0,0 +1,7 @@
+"""
+main_ndt_slam_server.py
+File for the offline implementation of NDT-SLAM on the Talbot 330 resident powerhouse machine
+Author: Ashwin Kanhere
+Date Created: 16th June 2019
+Date Modified: 16th June, 2019
+"""
