b9b54b3d5eec4305816c05b196d079ec888bfd66
Ashwin Kanhere <kanhereashwin@users.noreply.github.com>
2020-01-23 17:37:06 -0800

    Directory wide commit

--- implementation_laptop.py
+++
@@ -1,8 +0,0 @@
-"""
-implementation_laptop.py
-File for the offline implementation of NDT-SLAM
-Author: Ashwin Kanhere
-Date Created: 15th June 2019
-"""
-
-
--- kitti_verify.py
+++
@@ -1,9 +0,0 @@
-"""
-kitti_verify.py
-Test cases and functions to compare algorithm performance to KITTI ground truths
-Author: Ashwin Kanhere
-Date created: 31st May 2019
-"""
-
-# TODO: Write this function for the odometry sequences because ground truths aren't available for raw data
-
