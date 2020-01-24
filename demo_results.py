3b04aa81adc06f4178d65d4dbaec75f11c2fc807
Ashwin Kanhere <kanhereashwin@users.noreply.github.com>
2020-01-23 16:03:56 -0800

    First commit of demo_results.py

---
+++ demo_results.py
@@ -0,0 +1,7 @@
+"""
+demo_results.py
+Functions used to generate small examples and illustrations, primarily for presentations and papers
+Author: Ashwin Kanhere
+Date Created: 13th November 2019
+Date Modified: 13th November 2019
+"""
