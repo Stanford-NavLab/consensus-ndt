0d94471585d8c7c2163620799e1b0491dc6807d5
Ashwin Kanhere <kanhereashwin@users.noreply.github.com>
2020-02-01 17:47:03 -0800

    Added group meeting presentation code to demo_results.py

--- demo_results.py
+++ demo_results.py
@@ -15,6 +15,9 @@ import diagnostics
 from matplotlib import pyplot as plt
 import pykitti
 import lidar_mods
+import time
+import odometry
+
 
 def plot_cv():
     kitti_pcs = data_utils.load_kitti_pcs(0, 60, pc_mode='laptop')
@@ -94,6 +97,85 @@ def fault_consensus():
             #print('Plotting both point clouds')
             #diagnostics.plot_consec_pc(prev_pc, lidar_mod_trans_pc)
     return None
+
+
+def check_parameter_sizes():
+    pcs = data_utils.load_kitti_pcs(0, 10)
+    ref_pc = pcs[0]
+    pc_sizes = np.size(ref_pc)
+    print('Computing NDT with no overlap')
+    no_overlap_ndt = ndt.ndt_approx(ref_pc, horiz_grid_size=0.5, vert_grid_size=0.5, type='nooverlap')
+    print('Computing NDT for overlapping case')
+    overlap_ndt = ndt.ndt_approx(ref_pc, horiz_grid_size=0.5, vert_grid_size=0.5, type='overlapping')
+    print('Computing NDT for interpolated case')
+    interpolate_ndt = ndt.ndt_approx(ref_pc, horiz_grid_size=0.5, vert_grid_size=0.5, type='interpolate')
+    no_overlap_ndt.find_integrity(ref_pc)
+    no_overlap_ndt.filter_voxels_integrity(integrity_limit=0.7)
+
+    overlap_ndt.find_integrity(ref_pc)
+    overlap_ndt.filter_voxels_integrity(integrity_limit=0.7)
+
+    interpolate_ndt.find_integrity(ref_pc)
+    interpolate_ndt.filter_voxels_integrity(integrity_limit=0.7)
+
+    no_overlap_sizes = 9*len(no_overlap_ndt.stats)
+    overlap_sizes = 9*len(overlap_ndt.stats)
+    interpolate_sizes = 9*len(interpolate_ndt.stats)
+    print('Point cloud size is ', pc_sizes)
+    print('No overlap size is ', no_overlap_sizes)
+    print('Overlapping size is ', overlap_sizes)
+    print('Interpolated size is ', interpolate_sizes)
+    return None
+
+
+def group_meeting_demo():
+
+    #run_mode = 'server'
+    run_mode = 'laptop'
+    total_iters = 20
+    iter1 = 10
+    iter2 = 10
+
+    print('Loading dataset')
+    pcs = data_utils.load_uiuc_pcs(0, 10, mode=run_mode)
+
+    assert(total_iters == iter1 + iter2)
+
+    integrity_filters = np.array([0.5, 0.6, 0.7, 0.8])
+    ref_lidar = pcs[0]
+    ref_ndt = ndt.ndt_approx(ref_lidar)
+    perturb = np.array([0.2, 0.2, 0., 0., 0., 0.])
+    trans_pc = utils.transform_pc(perturb, ref_lidar)
+    ground_truth = utils.invert_odom_transfer(perturb)
+    error_consensus = np.zeros([np.size(integrity_filters), 1])
+    time_consensus = np.zeros_like(error_consensus)
+
+    print('Running baseline case')
+    tic = time.time()
+    new_odom = odometry.odometry(ref_ndt, trans_pc, max_iter_pre=total_iters, max_iter_post=0)
+    toc = time.time()
+    error_vanilla = np.linalg.norm(ground_truth - new_odom)
+    time_vanilla = toc - tic
+    # Save error and time values
+    print('The vanilla run error is ', error_vanilla)
+    print('The vanilla time taken is', time_vanilla)
+
+    for idx, filter_value in enumerate(integrity_filters):
+        print('Running case ', idx)
+        tic = time.time()
+        test_odom = odometry.odometry(ref_ndt, trans_pc, max_iter_pre=iter1, max_iter_post=iter2,
+                                      integrity_filter=filter_value)
+        toc = time.time()
+        error_consensus[idx] = np.linalg.norm(ground_truth - test_odom)
+        time_consensus[idx] = toc - tic
+        print('Error in run ', idx, ' is ', error_consensus[idx])
+        print('Time taken in run ', idx, ' is ', time_consensus[idx])
+
+    print('The consensus run errors are ', error_consensus)
+    print('The consensus run times are ', time_consensus)
+    print('The integrity filter values are ', integrity_filters)
+
+    return 0
 
 
 def old_run():
